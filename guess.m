e469390b6dfdd981925d8f55e0184de7cda29514
JemimaPoynton <91875592+JemimaPoynton@users.noreply.github.com>
2024-04-24 22:10:12 +0100

    Revert "Reapply "save""
    This reverts commit 4eb468deff1aa51a36617597937bcb83219cd3b6.

--- guess.m
+++ guess.m
@@ -3,14 +3,14 @@ function [z0, fval] = guess(func, lb, ub, n, Va)
 % of the global minima as an intial guess for z0 
 
 opts = optimoptions('particleswarm', 'FunctionTolerance', 1e-12, 'MaxIterations', 10000, 'SwarmSize', 700, 'ObjectiveLimit', 1e-6, 'MaxStallTime', 3);
-opts.MinNeighborsFraction = 0.5;
+% opts.MinNeighborsFraction = 0.5;
 
 fval = ones(1,6); z0 = ones(n,6);
-[z0(:,1), fval(1)] = particleswarm(func, n, lb, ub, opts);
+[z0(:,1), fval(1)] = particleswarm(func, n, lb(1:10), ub(1:10), opts);
 
 for i = 2:6
     if fval(i-1) > 1e-6
-        [z0(:,i), fval(:,i)] = particleswarm(func, n, lb, ub, opts);
+        [z0(:,i), fval(:,i)] = particleswarm(func, n, lb(1:10), ub(1:10), opts);
     else
         break
     end
