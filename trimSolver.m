3892d6e2231e965156e80e207b073a24157c3336
JemimaPoynton <91875592+JemimaPoynton@users.noreply.github.com>
2024-04-24 22:10:17 +0100

    Reapply "save"
    This reverts commit f1c564ffcdf54629a02855ce66d0d87bc5459c07.

--- trimSolver.m
+++ trimSolver.m
@@ -23,18 +23,21 @@ func = @(Z)trimCost(Z, Va, coeff, rho, refGeo, m, thrustIn, cg, I, traj, rpitch)
 %% Define Saturation
 uwRatio = atan(alphaLim);
 
-lb = [0 -0.25*Va -Va*uwRatio -pi/4 -pi/4 -pi/4 -0.52 -0.52 -0.52 0 0 0 0]';
-ub = [+Va +0.25*Va +Va*uwRatio +pi/4 +pi/4 +pi/4 0.52 0.52 0.52 7000 7000 7000 7000]';
+lb = [0 -Va*uwRatio -pi/4 -pi/4 -0.52 -0.52 -0.52 0 0 0 0]';
+ub = [+Va +Va*uwRatio +pi/4 +pi/4 0.52 0.52 0.52 7000 7000 7000 7000]';
+
+lb_ga = [0 -Va*uwRatio -pi/4 -pi/4 -0.52 -0.52 -0.52 0]';
+ub_ga = [+Va +Va*uwRatio +pi/4 +pi/4 0.52 0.52 0.52 7000]';
 
 %% Solve
-opts = optimset('PlotFcns','optimplotfval','TolX',1e-60, 'MaxIter', 100000, 'MaxFunEvals', 100000);
+opts = optimset('PlotFcns','optimplotfval', 'Algorithm', 'sqp');
 
 z0p_func = @(Z)trimCost_ga(Z, Va, coeff, rho, refGeo, m, thrustIn, cg, I, traj, rpitch);
-z0p = guess(z0p_func, lb, ub, 10, Va)';
+z0p = guess(z0p_func, lb_ga, ub_ga, 8, Va)';
 
-z0 = zeros(1,13)';
-z0(1:9) = z0p(1:9);
-z0(end-3:end) = z0p(10);
+z0 = zeros(1,11)';
+z0(1:7) = z0p(1:7);
+z0(end-3:end) = z0p(end);
 
 [Z, fval] = fmincon(func, z0, [], [], [], [], lb, ub, [], opts);
 
@@ -46,13 +49,14 @@ z0(end-3:end) = z0p(10);
 % end
 
 %% Check Convergence
-X = [Z(1) 0 Z(3)' 0 0 0 Z(4:6)']';
-U = [Z(7:9)' Z(10) 0 rpitch 0 ...
-            Z(11) 0 rpitch 0 ...
-            Z(12) 0 rpitch 0 ...
-            Z(13) 0 rpitch 0]';
+U = [Z(5:7)' Z(8) 0 rpitch 0 ...
+            Z(9) 0 rpitch 0 ...
+            Z(10) 0 rpitch 0 ...
+            Z(11) 0 rpitch 0]';
 
-if abs(fval) < 1e-3
+X = [Z(1) 0 Z(2)' 0 0 0 Z(3) 0 Z(4)]';
+
+if abs(fval) < 1e-6
     disp(['Successfully trimmed at ' mat2str(X)])
 else
     disp(['Unable to trim at ' mat2str(X)])
@@ -68,6 +72,8 @@ end
 V = sqrt(X(1)^2 + X(2)^2 + X(3)^2);
 alpha = atan2(X(3), X(1));
 beta = atan2(X(2), X(1));
+
+X(8) = alpha + traj(1);
 
 [Fb, Mcg, forces, aero, uvw_e, MTcg] = aeroDyn(coeff, U', alpha, beta, V, rho, X, refGeo, m, thrustIn, cg);
 end
