9c9773b167fccfd3d4e287f6270f36a8f0e5c0e5
JemimaPoynton <91875592+JemimaPoynton@users.noreply.github.com>
2024-04-26 21:53:10 +0100

    commiu

--- selectQRfull.m
+++ selectQRfull.m
@@ -5,28 +5,29 @@ X = reshape(trim.X,size(trim.X,1),[]); % reshape to linear indices (no stages)
 U = reshape(trim.U,size(trim.U,1),[]);
 Ut = [U(1:3,:); reshape(trim.Ut, size(trim.Ut,1),[])];
 
-idx = [8, 20, 30, 41; 
-       15, 21, 36, 55]; % indices of analysis points (1 sample from each mode)
+idx = [8, 22, 32, 41; 
+       15, 25, 36, 55]; % indices of analysis points (1 sample from each mode)
 
 sysMat = lineariseTrimFull(VX4, referenceGeo, coefficients, 'trimUAM1', 1e-5);
 
-% interpolate between trim points
-for i = 1:size(X,1)
-    Xint(i,:) = interp1(1:1:size(X,2), X(i,:), linspace(1,size(X,2), 900));
-end
-
 Xes = X(:,1);
 
-%% Iterate
+%% Iterate for LQI
+sysMat_LQI = augmentSys(sysMat);
+[Q, R] = solveQR_LQI(sysMat_LQI, idx, X, Ut);
+
+%% Save Results in File
+save('QRvalsLQI.mat', 'Q', 'R', 'idx')
+
+%% Iterate for LQR
 [Q, R] = solveQR(sysMat, idx, X, Ut);
 
 %% Save Results in File
-save('QRvalsEdgeCases.mat', 'Q', 'R', 'idx')
+save('QRvalsLQR.mat', 'Q', 'R', 'idx')
 
 %% Create Gain Matrix
-K = createGainMat(sysMat, 'QRvalsEdgeCases.mat', 'trimUAM1');
-
-% X = Xint;
+Kpi = createGainMat(sysMat_LQI, 'QRvalsLQI.mat', 'trimUAM1');
+K = createGainMat(sysMat, 'QRValsLQR', 'trimUAM1');
 
 %% Mission Runtime
 missionruntime = mission;
@@ -35,10 +36,10 @@ missionruntime = rmfield(missionruntime, 'rotorTilt');
 
 %% 
 t = linspace(0, mission.secTime(end), size(X,2));
-tl = linspace(0, mission.secTime(end), 3000);
+tl = linspace(0, mission.secTime(end), 10000);
 
-xvec = zeros(9,3000);
-uvec = zeros(9,3000);
+xvec = zeros(9,10000);
+uvec = zeros(9,10000);
 
 for i = 1:9
     xvec(i,:) = interp1(t, X(i,:), tl);
@@ -48,4 +49,4 @@ for i = 1:9
     uvec(i,:) = interp1(t, Ut(i,:), tl);
 end
 
-%% Plot Trajectory Tracking
+Xes = X(:,1);
