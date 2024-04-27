57e6ce8c48109ff3fc4ab7a256c5b6278291041b
JemimaPoynton <91875592+JemimaPoynton@users.noreply.github.com>
2024-04-27 14:36:56 +0100

    debugging

--- aeroDyn_MF.m
+++ aeroDyn_MF.m
@@ -63,8 +63,8 @@ Cn = applyDeriv(0, coeff.Cna, coeff.Cnb, coeff.Cnn, coeff.Cnp, 0, coeff.Cnr, u, 
 
 if abs(alpha) > 14.5*(pi/180)
     Cm = 0;
-    Cl = 0;
-    Cn = 0;
+    Cn = applyDeriv(0, 0, 0, coeff.Cnn, coeff.Cnp, 0, coeff.Cnr, u, alpha, beta, pqr, ndb);
+    Cl = applyDeriv(0, 0, 0, coeff.Cln, coeff.Clp, 0, coeff.Clr, u, alpha, beta, pqr, ndb);
 end
 
 Ma = [b*Cl; c*Cm; b*Cn].*Q*S;
